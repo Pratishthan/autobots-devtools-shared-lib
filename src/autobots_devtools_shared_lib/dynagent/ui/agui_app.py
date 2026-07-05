cf614b451474eb3a0654a43cc4e387b9dfa8a005
PK <pro.kamath@gmail.com>
2026-07-05 18:28:14 +0530

    Using 3001 and handling State delta w.r.t Run Finished.

--- src/autobots_devtools_shared_lib/dynagent/ui/agui_app.py
+++ src/autobots_devtools_shared_lib/dynagent/ui/agui_app.py
@@ -30,8 +30,8 @@ if TYPE_CHECKING:
 logger = get_logger(__name__)
 
 _DEFAULT_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
     "http://localhost:5173",
     "http://localhost:8080",
 ]
